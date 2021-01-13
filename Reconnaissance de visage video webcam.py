a83fddbc240536c8447759c774c2ac75297ca83b
felix068 <77407722+felix068@users.noreply.github.com>
2021-01-13 19:12:53 +0100

    Reconnaissance de visage video webcam.py
    Reconnaissance de visage video webcam.py

---
+++ Reconnaissance de visage video webcam.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+#       Fichier reconnaissance   #
+cascadeClassifierPath = 'haarcascade_frontalface_alt.xml'
+cascadeClassifier = cv2.CascadeClassifier(cascadeClassifierPath)
+cap = cv2.VideoCapture(0)
+
+
+#       Reconnaissance de visage         #
+print("Veuillez Patienter ...")
+while(cap.isOpened()):
+	_, frame = cap.read()
+	grayImage = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	detectedFaces = cascadeClassifier.detectMultiScale(grayImage,  scaleFactor=1.1, minNeighbors=10, minSize=(18, 18))
+	for(x,y, width, height) in detectedFaces:
+		cv2.rectangle(frame, (x, y), (x+width, y+height), (0,255,0), 3)
+#        Affichage du résultat       #
+	cv2.imshow("video", frame)
+	if cv2.waitKey(1) == ord('q'):
+		break
+
+#        Fin       #
+cap.release()
+cv2.destroyAllWindows()
